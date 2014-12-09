5bbfd5c0e213b942fd83ced022dcde42a90bd07f
filler56789 <wsm.9t6@gmail.com>
2014-12-09 10:43:24 +0000

    Update main.c

--- main.c
+++ main.c
@@ -634,9 +634,24 @@ static int32_t dcaenc_main(int32_t argc, char *argv[])
 	return 0;
 }
 
+#if defined(_WIN32) || defined(_WIN64)
+
+#include <Windows.h>
+#include <io.h>
+#include <fcntl.h>
+
+int32_t main( int32_t argc, char **argv )
+{
+	_setmode(_fileno(stdin),  _O_BINARY);
+	_setmode(_fileno(stdout), _O_BINARY);
+	return dcaenc_main(argc, argv);
+}
+
+#else
 
 int32_t main( int32_t argc, char **argv )
 {
 	return dcaenc_main(argc, argv);
 }
 
+#endif
